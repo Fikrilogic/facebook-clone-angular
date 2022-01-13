50001ac4d524c6a9ec24d5150bd6089f01b75f0b
Fikrilogic <fikrisandi3074@gmail.com>
2022-01-13 09:03:53 +0700

    fixed firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'laptop-shop-ef2df',
-    appId: '1:588598528848:web:00d46ba7237cf48a95fa55',
-    storageBucket: 'laptop-shop-ef2df.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyDvQuCTIOgmL8h9aVzja-Yf0e_OjtaZoSw',
-    authDomain: 'laptop-shop-ef2df.firebaseapp.com',
-    messagingSenderId: '588598528848',
-    measurementId: 'G-QQW7MS0MNZ',
-  },
+
   production: false
 };
 
